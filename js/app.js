eac63e1c0de701f6e95581bd300e976eb5d3d465
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2015-08-18 10:37:27 -0400

    Fine-tuned app data so it included multi-dimensional arrays

--- js/app.js
+++ js/app.js
@@ -1,68 +1,1 @@
-//=require ../components/angular/angular.min.js
-
-var eeveeQuiz = angular.module('eeveeApp', []);
-
-eeveeQuiz.controller("quizData", ["$scope", function($scope) {
-	$scope.questions = [
-		{
-			'number': '1',
-			'question': 'What\'s your favorite color?',
-			'answers':[
-				{
-					'text': 'Red',
-					'value': 'Flareon'
-				},{
-					'text': 'Blue',
-					'value': 'Vaporeon'
-				},{
-					'text': 'Yellow',
-					'value': 'Jolteon'
-				}
-			]
-		}, {
-			'number': '2',
-			'question': 'What\'s your favorite color?',
-			'answers':[
-				{
-					'text': 'Red',
-					'value': 'Flareon'
-				},{
-					'text': 'Blue',
-					'value': 'Vaporeon'
-				},{
-					'text': 'Yellow',
-					'value': 'Jolteon'
-				}
-			]
-		}
-	], $scope.results = [
-		{
-			'name': 'Eevee',
-			'type': 'normal'
-		}, {
-			'name': 'Vaporeon',
-			'type': 'water'
-		}, {
-			'name': 'Jolteon',
-			'type': 'electric'
-		}, {
-			'name': 'Flareon',
-			'type': 'fire'
-		}, {
-			'name': 'Espeon',
-			'type': 'psychic'
-		}, {
-			'name': 'Umbreon',
-			'type': 'dark'
-		}, {
-			'name': 'Glaceon',
-			'type': 'ice'
-		}, {
-			'name': 'Leafeon',
-			'type': 'grass'
-		}, {
-			'name': 'Sylveon',
-			'type': 'fairy'
-		}
-	];
-}])
+//=require ../components/angular/angular.min.jsvar eeveeQuiz = angular.module('eeveeApp', []);eeveeQuiz.controller("quizData", ["$scope", function($scope) {	$scope.questions = [		{			'number': '1',			'question': 'What\'s your favorite color?',			'answers':[				{					'text': 'Red',					'value': 'Flareon'				},{					'text': 'Blue',					'value': 'Vaporeon'				},{					'text': 'Yellow',					'value': 'Jolteon'				}			]		}, {			'number': '2',			'question': 'What\'s your favorite color?',			'answers':[				{					'text': 'Red',					'value': 'Flareon'				},{					'text': 'Blue',					'value': 'Vaporeon'				},{					'text': 'Yellow',					'value': 'Jolteon'				}			]		}	], $scope.results = [		{			'name': 'Eevee',			'type': 'normal'		}, {			'name': 'Vaporeon',			'type': 'water'		}, {			'name': 'Jolteon',			'type': 'electric'		}, {			'name': 'Flareon',			'type': 'fire'		}, {			'name': 'Espeon',			'type': 'psychic'		}, {			'name': 'Umbreon',			'type': 'dark'		}, {			'name': 'Glaceon',			'type': 'ice'		}, {			'name': 'Leafeon',			'type': 'grass'		}, {			'name': 'Sylveon',			'type': 'fairy'		}	];}])
