9e4fc7ece2096a30293ce54fe09297af15d0ca31
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2015-08-28 08:14:08 -0400

    Added up to 15 questions

--- js/notes.js
+++ js/notes.js
@@ -1,11 +1,11 @@
 /* index key:
-0 - Eevee: 3
-1 - Vaporeon: 3
-2 - Jolteon: 3
-3 - Flareon: 2
-4 - Espeon: 3
-5 - Umbreon: 3
-6 - Leafeon: 4
-7 - Glaceon: 2
-8 - Sylveon: 3
+0 - Eevee: 8
+1 - Vaporeon: 7
+2 - Jolteon: 8
+3 - Flareon: 7
+4 - Espeon: 7
+5 - Umbreon: 8
+6 - Leafeon: 7
+7 - Glaceon: 8
+8 - Sylveon: 8
 */
