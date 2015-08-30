b06149aed83df781622f42c9f216957d7498da4c
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2015-08-30 13:54:18 -0400

    Added Sass loop to create the basic structure for the winning parallax

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ var gulp = require('gulp'),
 
 var express = require('express')
 var app = express()
-app.use('/', express.static(__dirname + '/build'));
+app.use('/', express.static(__dirname + ''));
 app.listen(3000)
 console.log('Express site on 3000!')
 
