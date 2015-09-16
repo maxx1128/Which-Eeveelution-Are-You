a74dbfe28939d65f985f436f7dfe776f6e661610
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2015-09-16 13:19:02 -0400

    jQuery and Angular inline documentation

--- js/apps/functions.js
+++ js/apps/functions.js
@@ -1,31 +1,38 @@
+// The filter to show the current question only as they're being answered
 $scope.isStatus = function(question){
     return (question.number == $scope.q.current);
 };
 
+// The function for quickly updating the progress bar text in the progressText array
 progressCheck = function(point){
   return $scope.progressText[point].text;
 };
 
+// Setup any variables right away
+$scope.buttons.text = $scope.buttons.start; // Set the initial button text
+$scope.progress = progressCheck(0); // Set the initial progress text
+
+// This function runs whenever a question is answered
 $scope.answer = function(number) {
-  $scope.results[number].value++ ;
+  $scope.results[number].value++ ; // Adds the selected value to the results
 
-  if ( $scope.q.current == ($scope.q.left - 1) ) {
-  	$scope.buttons.start = $scope.buttons.end;
+  if ( $scope.q.current == ($scope.q.left - 1) ) { //When the user is at the last question
+  	$scope.buttons.start = $scope.buttons.end; //Changes the button text for the last question to the end text
   }
 
-  if($scope.q.left > $scope.q.current) {
-  	$scope.q.current++ ;
-  } else if ($scope.q.left == $scope.q.current) {
-  	$scope.view.quizOver = true;
+  if($scope.q.left > $scope.q.current) { // If there's still questions left to answer
+  	$scope.q.current++ ; // Goes to the next question
+  } else if ($scope.q.left == $scope.q.current) { // If they're at the last question when answering
+  	$scope.view.quizOver = true; // Goes to the results page
   }
 
-  if ($scope.q.current > ($scope.q.left - 2)) {
+  if ($scope.q.current > ($scope.q.left - 2)) { // At the last two questions
+    $scope.progress = progressCheck(4);
+  } else if ($scope.q.current > ($scope.q.left / 1.5)) { // Three quarters of the way through the quiz
     $scope.progress = progressCheck(3);
-  } else if ($scope.q.current > ($scope.q.left / 1.5)) {
+  } else if ($scope.q.current > ($scope.q.left / 2)) { // Halfway through the quiz
     $scope.progress = progressCheck(2);
-  } else if ($scope.q.current > ($scope.q.left / 2)) {
+  } else if ($scope.q.current > ($scope.q.left / 4)) { // One fourth of the way through the quiz
     $scope.progress = progressCheck(1);
-  } else if ($scope.q.current > ($scope.q.left / 4)) {
-    $scope.progress = progressCheck(0);
   }
 };
--- js/apps/variables.js
+++ js/apps/variables.js
@@ -1,30 +1,31 @@
 // Integer Values
-$scope.q = {
-	'left' : $scope.questions.length,
-	'totalPoints' : 20,
-	'current' : 1
+$scope.q = { // Number values related to the questions
+	'left' : $scope.questions.length, // The total number of questions, based on the data array
+	'current' : 1 // The current number of questions, incremented as the quiz goes on7
 };
 
 // Boolean values
 $scope.view = {
-	'quizOver': false,
-	'quizBegin': false
+	'quizOver': false, // If the quiz is over, to show the results page
+	'quizBegin': false // If the quiz was started, to show the questions
 };
 
 // String Values
-$scope.progress = 'Still a long way to go...';
-
+$scope.progress = ''; // The text on the quiz button
 $scope.progressText = [{
-		'text': 'Getting closer...'
+		'text':	'Still a long way to go...' // Initial progress text
 	}, {
-		'text': 'You\'re halfway there!'
+		'text': 'Getting closer...' //Progress text for 1/4 progress
 	}, {
-		'text': 'Keep going!'
+		'text': 'You\'re halfway there!' //Progress text for 1/2 progress
 	}, {
-		'text': 'The home stretch!'
+		'text': 'Keep going!' //Progress text for 3/4 progress
+	}, {
+		'text': 'The home stretch!' //Progress text for last two questions
 }];
 
 $scope.buttons = {
-	'start': 'Next Question',
-	'end': 'Finish Quiz!'
+	'text': '', // Blank variable for button text 
+	'start': 'Next Question', // Initial button text, for most questions
+	'end': 'Finish Quiz!' // Button text for last questions
 };
