e5a82131c8f08a66a85bf1fc16b53a2feb5556a9
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2015-09-19 16:27:27 -0400

    Fixed an extra Umbreon answer and added the results copy for each eeveelution

--- js/apps/data.js
+++ js/apps/data.js
@@ -17,9 +17,6 @@ $scope.questions = [
 				'text': 'Rainy',
 				'index': 1
 			}, {
-				'text': 'Dark and cloudy',
-				'index': 5
-			}, {
 				'text': 'Lots of sunshine!',
 				'index': 3
 			}, {
@@ -464,7 +461,7 @@ $scope.questions = [
 				'text': 'Cooking',
 				'index': 3
 			}, {
-				'text': 'Gardening',
+				'text': 'Gardening and/or hiking',
 				'index': 6
 			}, {
 				'text': 'Anything with lots of traveling',
@@ -658,6 +655,9 @@ $scope.questions = [
 			}, {
 				'text': 'Lots of different topics, one after the other',
 				'index': 2
+			}, {
+				'text': 'Dating? I barely do any dating, if any at all',
+				'index': 5
 			}
 		]
 	}, {
@@ -713,46 +713,46 @@ $scope.questions = [
 		'name': 'Eevee',
 		'type': 'Normal',
 		'value': 0,
-		'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum bibendum purus nec consequat ultrices. Vivamus quis risus placerat, scelerisque libero ac, interdum nisl. Proin eu ex turpis. Mauris volutpat, arcu quis fringilla aliquet, massa justo volutpat eros, sed aliquet sem nisi id nisl. Sed sodales eros ipsum. Nam sit amet volutpat lorem. Maecenas vitae pulvinar diam. Suspendisse varius quam et ex rutrum, vel efficitur metus suscipit. Cras a efficitur nulla. Quisque at est lacinia, condimentum urna vel, pharetra ex. Donec interdum porttitor tempor.'
+		'description': 'You\'re a very well-rounded and open-minded person, willing to hear new perspectives and not limit yourself to a set path. Like Eevee itself, the future is open to many possibilities! Take advantage of this by being as adventurous as possible, and remember to take some risks! However, you likely have a habit of being indecisive or naive at important moments. Don\'t be afraid to stand up for your beliefs and put your foot down!. As time goes and your identity takes greater shape, you have to be willing to fight for who you are.'
 	}, {
 		'name': 'Vaporeon',
 		'type': 'Water',
 		'value': 0,
-		'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum bibendum purus nec consequat ultrices. Vivamus quis risus placerat, scelerisque libero ac, interdum nisl. Proin eu ex turpis. Mauris volutpat, arcu quis fringilla aliquet, massa justo volutpat eros, sed aliquet sem nisi id nisl. Sed sodales eros ipsum. Nam sit amet volutpat lorem. Maecenas vitae pulvinar diam. Suspendisse varius quam et ex rutrum, vel efficitur metus suscipit. Cras a efficitur nulla. Quisque at est lacinia, condimentum urna vel, pharetra ex. Donec interdum porttitor tempor.'
+		'description': 'You have a very flexible personality that can easily roll with the punches and hardly be caught off guard. If things don\'t go according to plan, you\'ll likely just yell "Plot Twist!" and keep going. This gives you many different sides to your personality, but be careful. You have a tendency to be inconsistent with your goals, so have some more self-discipline! Don\'t let the people or circumstances around you control you too much, otherwise it\'ll be really hard to get what you want in life.'
 	}, {
 		'name': 'Jolteon',
 		'type': 'Electric',
 		'value': 0,
-		'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum bibendum purus nec consequat ultrices. Vivamus quis risus placerat, scelerisque libero ac, interdum nisl. Proin eu ex turpis. Mauris volutpat, arcu quis fringilla aliquet, massa justo volutpat eros, sed aliquet sem nisi id nisl. Sed sodales eros ipsum. Nam sit amet volutpat lorem. Maecenas vitae pulvinar diam. Suspendisse varius quam et ex rutrum, vel efficitur metus suscipit. Cras a efficitur nulla. Quisque at est lacinia, condimentum urna vel, pharetra ex. Donec interdum porttitor tempor.'
+		'description': 'You have no shortage of energy, as life is a race and you\'re always charging ahead at full speed! When you\'re excited for something you have plenty of enthusiasm, and that\'ll energize everyone around you too. But keep in mind that while you\'re great at doing things short-term, you need to keep the bigger picture of your life in mind too. Don\'t forget to slow down, pay attention to the details, and make some longer goals for yourself. Otherwise all that energy you have will burn out far too fast!'
 	}, {
 		'name': 'Flareon',
 		'type': 'Fire',
 		'value': 0,
-		'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum bibendum purus nec consequat ultrices. Vivamus quis risus placerat, scelerisque libero ac, interdum nisl. Proin eu ex turpis. Mauris volutpat, arcu quis fringilla aliquet, massa justo volutpat eros, sed aliquet sem nisi id nisl. Sed sodales eros ipsum. Nam sit amet volutpat lorem. Maecenas vitae pulvinar diam. Suspendisse varius quam et ex rutrum, vel efficitur metus suscipit. Cras a efficitur nulla. Quisque at est lacinia, condimentum urna vel, pharetra ex. Donec interdum porttitor tempor.'
+		'description': 'You rarely let anyone or anything hold you back, as the world is your playground and you\'re enjoying the games as much as possible. What\'s important is seeing the outside world and finding new experiences. Chances are you have either great stories to share or ambitious plans to put together. However, it\'s easy for you to get carried away and wind up lost in the woods of life. Adventure is good, but don\'t cross the line into aimless wandering, not knowing where to go less. Try to take a more mature approach to life, set a couple limits, and set aside some "serious" time every week.'
 	}, {
 		'name': 'Espeon',
 		'type': 'Psychic',
 		'value': 0,
-		'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum bibendum purus nec consequat ultrices. Vivamus quis risus placerat, scelerisque libero ac, interdum nisl. Proin eu ex turpis. Mauris volutpat, arcu quis fringilla aliquet, massa justo volutpat eros, sed aliquet sem nisi id nisl. Sed sodales eros ipsum. Nam sit amet volutpat lorem. Maecenas vitae pulvinar diam. Suspendisse varius quam et ex rutrum, vel efficitur metus suscipit. Cras a efficitur nulla. Quisque at est lacinia, condimentum urna vel, pharetra ex. Donec interdum porttitor tempor.'
+		'description': 'You\'re a major intellectual, almost always looking to learn more about the world, your passions, and anything interesting. It\'s very hard for others to fool you, or for important details to slip past your memory. Curiousity and self-motivation are always driving you to keep learning. But don\'t let your relentless pursuit of knowledge keep you from other experiences too. Put that knowledge to use every now and then and pursue new experiences you\'re not familiar with. Don\'t worry if you\'re nervous, no matter what happens you\'ll think of a way to handle it.'
 	}, {
 		'name': 'Umbreon',
 		'type': 'Dark',
 		'value': 0,
-		'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum bibendum purus nec consequat ultrices. Vivamus quis risus placerat, scelerisque libero ac, interdum nisl. Proin eu ex turpis. Mauris volutpat, arcu quis fringilla aliquet, massa justo volutpat eros, sed aliquet sem nisi id nisl. Sed sodales eros ipsum. Nam sit amet volutpat lorem. Maecenas vitae pulvinar diam. Suspendisse varius quam et ex rutrum, vel efficitur metus suscipit. Cras a efficitur nulla. Quisque at est lacinia, condimentum urna vel, pharetra ex. Donec interdum porttitor tempor.'
+		'description': 'You\'re a a very private person, with lots of secrets and a complex mind. You\'re fine operating mostly alone and can handle almost any situation by yourself, even when most people would want someone they knew nearby. Take advantage of this and push yourself to your limits whenever possible! However, just because you feel okay living life alone doesn\'t mean you should. You can easily get too withdrawn from others and feel lonely from time to time. It\'s hard, but try to put yourself out there more so you can have some more friends nearby for your next adventure.'
 	}, {
 		'name': 'Leafeon',
 		'type': 'Grass',
 		'value': 0,
-		'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum bibendum purus nec consequat ultrices. Vivamus quis risus placerat, scelerisque libero ac, interdum nisl. Proin eu ex turpis. Mauris volutpat, arcu quis fringilla aliquet, massa justo volutpat eros, sed aliquet sem nisi id nisl. Sed sodales eros ipsum. Nam sit amet volutpat lorem. Maecenas vitae pulvinar diam. Suspendisse varius quam et ex rutrum, vel efficitur metus suscipit. Cras a efficitur nulla. Quisque at est lacinia, condimentum urna vel, pharetra ex. Donec interdum porttitor tempor.'
+		'description': 'You\'re very conscious of your environment, both environmentally and understanding what\'s around you. Details most people would glaze over are often spotted by you, helping you be more resourceful and practical. Chances are you\'ve got plenty of great survival skills, and can do a lot more with a lot less than many others. Plus you try to leave your home, and planet, at least as healthy as it was when you arrived. Just don\'t get too caught up in what\'s already around you. Unleash your creative side and do some inventing to add to this world as well as keeping it healthy!'
 	}, {
 		'name': 'Glaceon',
 		'type': 'Ice',
 		'value': 0,
-		'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum bibendum purus nec consequat ultrices. Vivamus quis risus placerat, scelerisque libero ac, interdum nisl. Proin eu ex turpis. Mauris volutpat, arcu quis fringilla aliquet, massa justo volutpat eros, sed aliquet sem nisi id nisl. Sed sodales eros ipsum. Nam sit amet volutpat lorem. Maecenas vitae pulvinar diam. Suspendisse varius quam et ex rutrum, vel efficitur metus suscipit. Cras a efficitur nulla. Quisque at est lacinia, condimentum urna vel, pharetra ex. Donec interdum porttitor tempor.'
+		'description': 'You\'re a very composed and "in-control" person that\'s very hard to throw off balance. While there\'s almost always a way to get under someone\'s skin, people have a hard time messing with you. You know what you want, where you\'re going, and have all your priorities right where they should be. But while you have a good head on your shoulders, don\'t get stuck in it for too long. Otherwise you\'ll drift away from what\'s around you, getting cynical or mean-spirited in the process. So be sure how you deal with others is as consistent and healthy as how you deal with yourself.'
 	}, {
 		'name': 'Sylveon',
 		'type': 'Fairy',
 		'value': 0,
-		'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum bibendum purus nec consequat ultrices. Vivamus quis risus placerat, scelerisque libero ac, interdum nisl. Proin eu ex turpis. Mauris volutpat, arcu quis fringilla aliquet, massa justo volutpat eros, sed aliquet sem nisi id nisl. Sed sodales eros ipsum. Nam sit amet volutpat lorem. Maecenas vitae pulvinar diam. Suspendisse varius quam et ex rutrum, vel efficitur metus suscipit. Cras a efficitur nulla. Quisque at est lacinia, condimentum urna vel, pharetra ex. Donec interdum porttitor tempor.'
+		'description': 'People likely smile the second they see you, since you make almost everyone smile as well. Helping others, both friends and strangers, are high priorities as you try and help others as much as possible. You\'ll likely make a difference in many people\'s lives, and brighten up their days in the progress. While you make the world better for so many others though, obviously you can\'t forget your happiness as well. Make some "me time" a priority in your routine, since it\'ll be harder for you to keep helping others if you wither out. After all, others care for your happiness as much as you care for theirs. Don\'t let them down!'
 	}
 ];
